10ff85bf128a5e6283c313653d8d9ee2a2012d89
Kenny Howard <ken.chad.howard@rbs.com>
2015-01-26 16:40:00 -0700

    saving progress

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,37 @@
 var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
+	this.addNewGame = function(gameObject) {
+		url: 'https://api.parse.com/1/classes/' + gameObj.homeTeam,
+		if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+			gameObj.won = true;
+		} else {
+			gameObj.won = false;
+		}
+		return $http ({
+			method: 'POST',
+			url: url,
+			data: gameObj
+		})
+	}
+
+	this.getTeamData = function(team) {
+		var dfd = $q.defer();
+		$http({
+			method: 'GET',
+			url: 'https://api.parse.com/1/classes/' + team
+		}).then(function(data){
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0
+			for (var i = 0; i < results.length; i++) {
+				if (results[i].won === true) {
+					wins++;
+				} else {
+					losses++;
+				}
+			}
+		})
+	}
 
 });
